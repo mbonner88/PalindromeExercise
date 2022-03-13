4c503cf1438a7bd3a8064bad43d4c505eb287f6f
Matthew Bonner <matthewsbonner@comcast.net>
2022-03-13 12:27:54 -0400

    init commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var array = word.ToCharArray();
+            Array.Reverse(array);
+            var reverse = new string(array);
+            return (reverse == word) ? true : false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("tacocat", true)]
+        [InlineData("level", true)]
+        [InlineData("civic", true)]
+        [InlineData("madam", true)]
+        [InlineData("bacon", false)]
+        [InlineData("alfredo", false)]
+        [InlineData("flour", false)]
+        public void Test1(string word, bool expected)
         {
-
+            var test = new WordSmith();
+            var actual = test.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
